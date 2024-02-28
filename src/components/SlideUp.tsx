a73b130fd74a2ba4de506bae9981067ed3fb4f66
HeitorBMVanguard <heitorborba@sistemavanguard.com.br>
2024-02-28 17:41:53 -0300

    Atualizando slide up

--- src/components/SlideUp.tsx
+++ src/components/SlideUp.tsx
@@ -1,44 +1,36 @@
-'use client'
+"use client" // this is a client component
 
-import React, { useEffect, useRef, ReactNode } from "react";
-
+import React, { useEffect, useRef, ReactNode } from "react"
 interface Props {
-  offset?: string;
-  children?: ReactNode;
+  offset?: string
+  children?: ReactNode
+  // any props that come into the component
 }
 
 export default function SlideUp({ children, offset = "0px" }: Props) {
-  const ref = useRef(null);
+  const ref = useRef(null)
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.remove("opacity-0");
-            entry.target.classList.add("animate-slideUpCubiBezier");
+            entry.target.classList.remove("opacity-0")
+            entry.target.classList.add("animate-slideUpCubiBezier")
           }
-        });
+        })
       },
       { rootMargin: offset }
-    );
+    )
 
     if (ref.current) {
-      observer.observe(ref.current);
+      observer.observe(ref.current)
     }
-
-    // Cleanup function
-    return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
-      }
-    };
-  // Include `offset` in the dependency array if you need to re-run the effect when it changes.
-  }, [offset]); // This assumes `offset` can change and you want to re-setup the observer in response.
+  }, [ref])
 
   return (
     <div ref={ref} className="relative opacity-0">
       {children}
     </div>
-  );
+  )
 }
