f5318975982833230f06c3e7eb2db0b48d09a15d
HeitorBMVanguard <heitorborba@sistemavanguard.com.br>
2024-02-28 17:13:45 -0300

    Removendo mudança de tema do navbar e ajustando head para páginas web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 "use client"
-import '../app/globals.css'
+import "../app/globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import { ThemeProvider } from "next-themes"
+import Head from "./head"
 
 export default function RootLayout({
   children,
@@ -15,7 +16,7 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <Head />
       <body className="dark:bg-stone-900">
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
@@ -25,4 +26,4 @@ export default function RootLayout({
       </body>
     </html>
   )
-    }
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -81,21 +81,7 @@ export default function Navbar() {
                 </Link>
               );
             })}
-                {currentTheme === "dark" ? (
-                  <button
-                    onClick={() => setTheme("light")}
-                    className="bg-slate-300 p-1.5 rounded-xl"
-                  >
-                    <RiSunLine size={25} color="black" />
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => setTheme("dark")}
-                    className="bg-slate-100 p-1.5 rounded-xl"
-                  >
-                    <RiMoonFill size={25} />
-                  </button>
-                )}
+               
               </div>
             </div>
           </div>
